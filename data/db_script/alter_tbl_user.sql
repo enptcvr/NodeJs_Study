de5fbee66d7423006e6b589673224e58a82664da
devin.gong <devin.gong.hn@outlook.com>
2021-08-14 02:12:29 +0800

    add alter tbl_user table script

---
+++ data/db_script/alter_tbl_user.sql
@@ -0,0 +1,4 @@
+alter table tbl_user add column create_by varchar(64)
+alter table tbl_user add column create_date datetime
+alter table tbl_user add column modify_by varchar(64)
+alter table tbl_user add column modify_date datetime
