b7bf460d89cb2a7329affceff2f551478839abba
devin.gong <devin.gong.hn@outlook.com>
2021-08-14 00:06:40 +0800

    read port by process en

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ app.post("/",(req, res)=>{
     console.log("hello");
 })
 
+var port = process.env.port || 9977
 app.listen(9977,(err)=>{
     if(!err){
-    console.log("server started!")
+    console.log(`server started at ${port}!`)
     }else{
         console.log(err)
     }
