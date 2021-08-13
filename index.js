7ef2000a080103df1cb89adb68a7a0d3d00056da
devin.gong <devin.gong.hn@outlook.com>
2021-08-14 02:08:00 +0800

    use process env port

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require('express')
 
 const app = express()
 
-
 app.use('/',require('./data/route'))
 
-app.post("/",(req, res)=>{
-    console.log("hello");
+app.get("/",(req, res)=>{
+    res.send("<h1>Hello guys welcome to my nodejs study course!</h1>")
 })
 
 var port = process.env.port || 9977
-app.listen(9977,(err)=>{
+app.listen(port,(err)=>{
     if(!err){
     console.log(`server started at ${port}!`)
     }else{
